ff769fe87264bff88a418a8ec830655c1eb30082
toroitichcaroline27-sketch <toroitichcaroline27@gmail.com>
2026-08-19 14:48:16 +0300

    Add inventory product model

---
+++ src/main/java/com/backroad/model/InventoryProduct.java
@@ -0,0 +1,41 @@
+package com.backroad.model;
+
+public class InventoryProduct {
+
+    private String productId;
+    private String productName;
+    private int quantity;
+
+    public InventoryProduct() {
+    }
+
+    public InventoryProduct(String productId, String productName, int quantity) {
+        this.productId = productId;
+        this.productName = productName;
+        this.quantity = quantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
